49002a581390428cbdfb2fa50350f18803cdc4b5
our ame <you@example.com>
2019-11-10 18:19:02 +0700

    Подключил библиотеку

---
+++ src/components/Chart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="chart">
+    <h2>График</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { load, endpoints } from "../api";
+
+@Component
+export default class Items extends Vue {
+  @Prop() private keywords!: string[];
+
+  Result = [];
+  async mounted() {
+    
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>

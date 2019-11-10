d477d6ff52b121d5b00e7f1d6240187eed30756e
our ame <you@example.com>
2019-11-10 22:38:59 +0700

    Добавил ts код

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -3,3 +3,10 @@
     <b-navbar-brand href="#">Hh ru api testing</b-navbar-brand>
   </b-navbar>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+@Component
+export default class MyHeader extends Vue {
+}
+</script>

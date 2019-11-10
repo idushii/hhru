119f9846b861c85567b83bb378463d4bb98d6d5a
our ame <you@example.com>
2019-11-10 19:01:48 +0700

    Подключил вывод графиков

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,22 +1,70 @@
 <template>
   <div class="chart">
     <h2>График</h2>
+    <canvas id="myChart" width="400" height="100"></canvas>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-import { load, endpoints } from "../api";
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { load, endpoints, defaultParams, getUrl } from "../api";
+
+var Chart = require("chart.js");
+
+import { mapState } from "vuex";
 
 @Component
-export default class Items extends Vue {
+export default class MyChart extends Vue {
   @Prop() private keywords!: string[];
 
-  Result = [];
-  async mounted() {
-    
+  get items() {
+    return this.$store.getters["items/items"];
   }
 
+  get Values(): number[] {
+    let result: number[] = [];
+    for (let text of this.keywords) {
+      let url = getUrl(endpoints.vacancies, { ...defaultParams, text });
+      if (this.items[url]) result.push(this.items[url].found);
+    }
+    return result;
+  }
+
+  @Watch("Values")
+  ff() {
+    console.log("Values");
+    this.draw();
+  }
+
+  async draw() {
+    var ctx = document.getElementById("myChart");
+
+    var myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: this.keywords,
+        datasets: [
+          {
+            label: "# of Votes",
+            data: this.Values,
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
 }
 </script>
 

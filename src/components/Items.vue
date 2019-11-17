b68dc1edb248ef4639271362e38a091961c9b912
our ame <you@example.com>
2019-11-17 12:03:02 +0700

    Вынес  из items элемент item

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -2,16 +2,12 @@
   <b-card class="Items">
     <h2 v-if="Result" @click="showAll">{{search}}: {{Result.found}}</h2>
     <template v-if="isShowAll">
-      <div class="item" v-for="item in Result.items" :key="`item-${item.id}`">
-        <h3>{{item.name}}</h3>
-        <div v-if="item.address">{{item.address.city}}</div>
-        <div v-if="item.snippet">
-          <div v-html="item.snippet.requirement" />
-          <div v-html="item.snippet.responsibility" />
-        </div>
-        <a :href="item.alternate_url" target="_blank">Ссылка</a>
-        <div v-if="false">{{item}}</div>
-      </div>
+      <item v-for="item in Result.items" :key="`item-${item.id}`"  
+        :name="item.name" 
+        :alternate_url="item.alternate_url" 
+        :address="item.address" 
+        :snippet="item.snippet" 
+      />
     </template>
   </b-card>
 </template>
@@ -19,13 +15,19 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { load, endpoints } from '../api';
+import item from "./item.vue";
 
-@Component
+@Component({
+  components: {
+    item,
+  },
+})
 export default class Items extends Vue {
-
-  public Result = [];
   @Prop() private search!: string;
   @Prop() private isShowAll!: boolean;
+
+  public Result = [];
+
   public async mounted() {
     this.Result = await this.$store.dispatch('items/addItem', {
       type: endpoints.vacancies,
---
+++ src/components/item.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="item">
+    <h3>{{name}}</h3>
+    <div v-if="address">{{address.city}}</div>
+    <div v-if="snippet">
+      <div v-html="snippet.requirement" />
+      <div v-html="snippet.responsibility" />
+    </div>
+    <a :href="alternate_url" target="_blank">Ссылка</a>
+    <div v-if="false">{{item}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+interface IAddress {
+  city: string;
+}
+
+interface ISnippet {
+  city: string;
+}
+
+@Component
+export default class Item extends Vue {
+  @Prop() private name!: string;
+  @Prop() private alternate_url!: string;
+  @Prop() private address!: IAddress;
+  @Prop() private snippet!: ISnippet;
+}
+</script>
+
+<style lang="scss" scoped>
+.item {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  h3 {
+    margin-bottom: 5px;
+  }
+}
+</style>
